2ec8c37c51d66d9bf5428e8a50279a8acbb6198b
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-06-29 14:08:05 +0200

    One-dimenisonal array for books

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -4,23 +4,25 @@ import library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Library v0.5";
+        final String appName = "Library v0.7";
 
-        Book book1 = new Book("Java. Effective programming. Guide for the good developer",
+        Book[] books = new Book[1000];
+
+        books[0] = new Book("Java. Effective programming. Guide for the good developer",
                 "Joshua Bloch",
                 2009,
                 352,
                 "Helion",
                 "978-83-246-1829-1");
 
-        Book book2 = new Book("ASP.NET MVC 5. Professional programming in C#",
+        books[1] = new Book("ASP.NET MVC 5. Professional programming in C#",
                 "Wojciech Suwała",
                 2016,
                 1072,
                 "Helion",
                 "978-83-283-0234-8");
 
-        Book book3 = new Book("Thinking in Java",
+        books[2] = new Book("Thinking in Java",
                 "Bruce Eckel",
                 2005,
                 1168,
@@ -28,8 +30,8 @@ public class Library {
 
         System.out.println(appName);
         System.out.println("Books available in the library:");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
     }
 }
