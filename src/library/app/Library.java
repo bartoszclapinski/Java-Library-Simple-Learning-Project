05795c6d23a202e43f182b833959d103a97485f4
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-07-14 13:31:44 +0200

    App updated wit scanner

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -1,32 +1,19 @@
 package library.app;
 
+import library.io.DataReader;
 import library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Library v0.7";
+        final String appName = "Library v0.8";
+        DataReader dataReader = new DataReader();
 
         Book[] books = new Book[1000];
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        books[2] = dataReader.readAndCreateBook();
 
-        books[0] = new Book("Java. Effective programming. Guide for the good developer",
-                "Joshua Bloch",
-                2009,
-                352,
-                "Helion",
-                "978-83-246-1829-1");
-
-        books[1] = new Book("ASP.NET MVC 5. Professional programming in C#",
-                "Wojciech Suwała",
-                2016,
-                1072,
-                "Helion",
-                "978-83-283-0234-8");
-
-        books[2] = new Book("Thinking in Java",
-                "Bruce Eckel",
-                2005,
-                1168,
-                "Helion");
+        dataReader.close();
 
         System.out.println(appName);
         System.out.println("Books available in the library:");
---
+++ src/library/io/DataReader.java
@@ -0,0 +1,32 @@
+package library.io;
+
+import library.model.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Book readAndCreateBook() {
+        System.out.println("Title: ");
+        String title = scanner.nextLine();
+        System.out.println("Author: ");
+        String author = scanner.nextLine();
+        System.out.println("Release date: ");
+        int releaseDate = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Pages: ");
+        int pages = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Publisher: ");
+        String publisher = scanner.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = scanner.nextLine();
+
+        return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
